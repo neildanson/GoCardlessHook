9d8ed1276b3e819bb02e004d604de3375e6f513d
Neil Danson <neil.danson@gmail.com>
2025-03-14 15:44:57 +0000

    Try updating ports to ones that work in z2p

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.WebHost.UseUrls("http://0.0.0.0:8080/");
+        builder.WebHost.UseUrls("http://0.0.0.0:8000/");
 
         var app = builder.Build();
 
