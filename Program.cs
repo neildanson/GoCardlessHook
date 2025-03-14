cd163f0c03f61c4900409cd427aab218ce15035f
Neil Danson <neil.danson@gmail.com>
2025-03-14 15:29:03 +0000

    Configure web host to listen on port 8080
    Added configuration to allow the application to be accessible from all network interfaces by setting the web host URL to "http://0.0.0.0:8080/".

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.WebHost.UseUrls("http://0.0.0.0:8080/");
 
         var app = builder.Build();
 
