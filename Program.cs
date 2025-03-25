17d928d6b683f5be836fc8ce733c0027f1c200b1
Neil Danson <neil.danson@gmail.com>
2025-03-25 14:18:21 +0000

    8000 -> 8080

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.WebHost.UseUrls("http://0.0.0.0:8000/");
+        builder.WebHost.UseUrls("http://0.0.0.0:8080/");
         GoCardlessClient client = GoCardlessClient.Create(
                 // We recommend storing your access token in an
                 // configuration setting for security
