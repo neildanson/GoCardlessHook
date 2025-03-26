5ac90e9208bdf18bdf05bd9ffbbe39af0ec6cd6e
Neil Danson <neil.danson@gmail.com>
2025-03-26 16:39:36 +0000

    Change access

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ public class Program
             Console.WriteLine($"{key} : {ALL_ENVS[key]}");
         }
         Console.WriteLine("#####################################");
-        var API_KEY = Environment.GetEnvironmentVariables()["GO_CARDLESS_APIKEY"]?.ToString();
-        var SECRET = Environment.GetEnvironmentVariables()["GO_CARDLESS_WEBHOOKSECRET"]?.ToString();
+        var API_KEY = ALL_ENVS["GO_CARDLESS_APIKEY"]?.ToString();
+        var SECRET = ALL_ENVS["GO_CARDLESS_WEBHOOKSECRET"]?.ToString();
         Console.WriteLine($"API_KEY : {API_KEY}, SECRET {SECRET}");
 
         var builder = WebApplication.CreateBuilder(args);
